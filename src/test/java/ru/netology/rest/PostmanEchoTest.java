4a32c16c2befed2ae304122ed7120806885c943c
Polina <pzmujdeckaa@gmail.com>
2023-07-06 20:42:14 +0500

    artifacts

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,17 @@
+package ru.netology.rest;
+
+import....
+
+public class PostmanEchoTest {
+    @Test
+    void  shouldSendData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("H1")
+                .when()
+                .post( "/post")
+                .then()
+                .statucCode( 200)
+                .body("data", equalTo)( "H1"));
+    }
+}
